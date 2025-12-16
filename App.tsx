4c9f68540ecd537ed2711c13aa5331681bb461ea
Kundan <kundansingh957632@gmail.com>
2025-12-16 17:57:36 +0530

    updates

--- App.tsx
+++ App.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { Alert, SafeAreaView, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const App = () => {
 
+  const onMessage = (event: any) => {
+    let data = JSON.parse(event.nativeEvent.data);
+    if (data.type === 'GO_BACK') {
+      Alert.alert('Go Back', 'Are you sure you want to go back?', [
+        {
+          text: 'Yes',
+          onPress: () => console.log('Yes pressed'),
+        },
+        {
+          text: 'No',
+          onPress: () => console.log('No pressed'),
+          style: 'cancel',
+        },
+      ]);
+    }
+  }
+
+
+  
   return (
     <SafeAreaView style={styles.container}>
       <WebView
-        source={{ uri: "https://www.google.com" }}
+        source={{ uri: "http://localhost:5173" }}
         style={styles.webview}
         javaScriptEnabled
         domStorageEnabled
+        geolocationEnabled={true}
+        onMessage={onMessage}
       />
     </SafeAreaView>
   );
